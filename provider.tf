1bcef63fa363bcc656f542eb185fb65efef5782a
Chris Dunkin <cdunkin@hy-vee.com>
2020-03-21 21:44:51 -0500

    removes provider info that should be env vars

--- provider.tf
+++ provider.tf
@@ -1,7 +1,5 @@
 provider "azurerm" {
     version = "=2.0.0"
-    subscription_id = "0389e496-a9ad-4be0-8404-fa6fa1e18559"
-    tenant_id = "eda2122b-8eb0-4ea6-a069-f3b74f8f822c"
     features {}
 }
 
